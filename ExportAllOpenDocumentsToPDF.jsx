732a0449fb2229bb5bef8160fc74a7261def09c5
Wyrax <wyrax@ukr.net>
2020-10-23 17:58:14 +0300

    Base version for further development

---
+++ ExportAllOpenDocumentsToPDF.jsx
@@ -0,0 +1,15 @@
+﻿//InD script
+    $.writeln('app.documents.length = ' + app.documents.length);
+    var documentsNumber = app.documents.length;
+for (i = 0; i < documentsNumber; i++) {
+    $.writeln('i = ' + i);
+    var fileName = app.activeDocument.textFrames[0].contents; // Get text frame content
+    $.writeln('fileName = ' + fileName);
+    app.activeDocument.textFrames[0].remove();
+
+    var myPDFExportPreset = app.pdfExportPresets.item("Press Quality Sportlife");
+    app.activeDocument.exportFile(ExportFormat.pdfType, File("C:/Users/User/Desktop/PDFCatcher/"+fileName + ".pdf"), false, myPDFExportPreset);
+    $.writeln('app.activeDocument.name = ' + app.activeDocument.name);
+    app.activeDocument.close(SaveOptions.no);
+
+}
