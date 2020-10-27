d16eaa1cf616126f7dade32569e3a9f9cea1ac79
Wyrax <wyrax@ukr.net>
2020-10-27 17:53:47 +0200

    Checks if disk exists

--- ExportAllOpenDocumentsToPDF.jsx
+++ ExportAllOpenDocumentsToPDF.jsx
@@ -1,5 +1,10 @@
 ﻿// Adobe InDesign CC/2020 script that exports all open documents as PDFs using text data from latest added text frame as file name.
 
+var disk = new Folder('T:/');
+    if (!disk.exists) {
+        $.writeln(disk + "not exists");
+    }
+
 if (app.documents.length == 0) {
     alert('Open documents first.');
 } else {
@@ -8,31 +13,12 @@ if (app.documents.length == 0) {
     if (!outputFolder.exists) {
         outputFolder.create();
     }
-    
-        $.writeln('outputFolder.absoluteURI = ' + outputFolder.absoluteURI);
-        $.writeln('outputFolder.alias = ' + outputFolder.alias);
-        $.writeln('outputFolder.created = ' + outputFolder.created);
-        $.writeln('outputFolder.displayName = ' + outputFolder.displayName);
-        $.writeln('outputFolder.exist = ' + outputFolder.exist);
-        $.writeln('outputFolder.fsName = ' + outputFolder.fsName);
-        $.writeln('outputFolder.fullName = ' + outputFolder.fullName);
-        $.writeln('outputFolder.modified = ' + outputFolder.modified);
-        $.writeln('outputFolder.name = ' + outputFolder.name);
-        $.writeln('outputFolder.parent = ' + outputFolder.parent);
-        $.writeln('outputFolder.path = ' + outputFolder.path);
-        $.writeln('outputFolder.relativeURI = ' + outputFolder.relativeURI);
-        $.writeln('outputFolder.name = ' + outputFolder.name);
-    
-
     for (i = 0; i < documentsNumber; i++) {
         var fileName = app.activeDocument.textFrames[0].contents; // Get text frame content
         app.activeDocument.textFrames[0].remove();
-
         var myPDFExportPreset = app.pdfExportPresets.item("Press Quality Sportlife");
         app.activeDocument.exportFile(ExportFormat.pdfType, File(outputFolder + "/" + fileName + ".pdf"), false, myPDFExportPreset);
-        $.writeln(outputFolder + fileName + ".pdf");
         app.activeDocument.close(SaveOptions.no);
     }
     alert("Exported into " + outputFolder.fsName);
-
 }
