cf070ad3d541c30def21e8881e7bf1ae901e8306
Wyrax <wyrax@ukr.net>
2020-10-27 16:50:37 +0200

    Working version

--- ExportAllOpenDocumentsToPDF.jsx
+++ ExportAllOpenDocumentsToPDF.jsx
@@ -1,15 +1,15 @@
-﻿//InD script
-    $.writeln('app.documents.length = ' + app.documents.length);
+﻿// Adobe InDesign CC/2020 script that exports all open documents as PDFs using text data from latest added text frame as file name.
+
+if (app.documents.length == 0) {
+    alert('Open documents first.');
+} else {
     var documentsNumber = app.documents.length;
-for (i = 0; i < documentsNumber; i++) {
-    $.writeln('i = ' + i);
-    var fileName = app.activeDocument.textFrames[0].contents; // Get text frame content
-    $.writeln('fileName = ' + fileName);
-    app.activeDocument.textFrames[0].remove();
+    for (i = 0; i < documentsNumber; i++) {
+        var fileName = app.activeDocument.textFrames[0].contents; // Get text frame content
+        app.activeDocument.textFrames[0].remove();
 
-    var myPDFExportPreset = app.pdfExportPresets.item("Press Quality Sportlife");
-    app.activeDocument.exportFile(ExportFormat.pdfType, File("C:/Users/User/Desktop/PDFCatcher/"+fileName + ".pdf"), false, myPDFExportPreset);
-    $.writeln('app.activeDocument.name = ' + app.activeDocument.name);
-    app.activeDocument.close(SaveOptions.no);
-
+        var myPDFExportPreset = app.pdfExportPresets.item("Press Quality Sportlife");
+        app.activeDocument.exportFile(ExportFormat.pdfType, File("C:/Users/User/Desktop/PDFCatcher/"+fileName + ".pdf"), false, myPDFExportPreset);
+        app.activeDocument.close(SaveOptions.no);
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ExportAllOpenDocumentsToPDF
-Adobe InDesign CC/2020 script that exports all open documents as PDFs
+Adobe InDesign CC/2020 script that exports all open documents as PDFs using text data from latest added text frame as file name.
